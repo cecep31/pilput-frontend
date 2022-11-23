d48c45d30520bf0f357ec1103f74e3afbdbe472e
cecep <cecepjanuardi31@gmail.com>
2022-11-23 14:49:44 +0700

    improve speed

--- pages/dashboard/mytask.js
+++ pages/dashboard/mytask.js
@@ -1,6 +1,22 @@
 import React from "react";
 import Logged from "../../components/layouts/Logged";
+import nookies from 'nookies'
 
+export async function getServerSideProps(ctx) {
+  const cookies = nookies.get(ctx);
+  // console.log(cookies.token);
+  if (!cookies.token) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {}, // will be passed to the page component as props
+  };
+}
 function Mytask() {
   return (
   <Logged>
