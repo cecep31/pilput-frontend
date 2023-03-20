23ec014ef18db59a31b26a8fd70469b82c7c59e5
cecep31 <cecepjanuardi31@gmail.com>
2023-03-20 09:28:16 +0700

    input wrong

--- pages/chat/global.js
+++ pages/chat/global.js
@@ -53,7 +53,7 @@ const Global = () => {
           value={message}
           onChange={(e) => setmessage(e.target.value)}
           type="text"
-          className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+          className="rounded-lg border-transparent flex-1 appearance-none border border-gray-500 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
           placeholder="Message.."
         />
 
