a57501785bce5078d4b7a26ab918bc7630a4d004
cecep31 <cecepjanuardi31@gmail.com>
2023-03-24 10:32:08 +0700

    form websocket

--- pages/chat/global.js
+++ pages/chat/global.js
@@ -20,13 +20,15 @@ const Global = () => {
   }, [lastMessage]);
 
   const handleClickChangeSocketUrl = () => {
+    
     setSocketUrl(process.env.NEXT_PUBLIC_WS_HOST + "/ws/global");
+    console.log("reconnect seharusnya");
   };
-  async function handleClickSendMessage() {
+  async function handleClickSendMessage(event) {
+    event.preventDefault();
     await sendMessage(message);
     setMessageHistory((prev) => prev.concat({ data: message }));
     setmessage("");
-    // console.log(messageHistory);
   }
 
   const connectionStatus = {
@@ -41,40 +43,45 @@ const Global = () => {
     <div className="absolute top-0 bottom-0 right-0 left-0 mx-auto px-10 py-10">
       <button
         className="p-2 bg-green-300 rounded-lg hover:bg-green-600"
-        onClick={handleClickChangeSocketUrl}
+        onClick={handleClickSendMessage}
       >
         Try To coonect
       </button>
       <div className="text-gray-500">{connectionStatus}</div>
       <br></br>
 
-      <div className="relative flex">
-        <input
-          value={message}
-          onChange={(e) => setmessage(e.target.value)}
-          type="text"
-          className="rounded-lg border-transparent flex-1 appearance-none border border-gray-500 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-          placeholder="Message.."
-        />
+      <div >
+        <form className="relative flex" onSubmit={handleClickSendMessage}>
+          <input
+            value={message}
+            onChange={(e) => setmessage(e.target.value)}
+            type="text"
+            className="rounded-lg border-transparent flex-1 appearance-none border border-gray-500 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+            placeholder="Message.."
+          />
 
-        <button
-          className="border-2 rounded-lg border-gray-700 w-60 ml-2 hover:border-green-700"
-          onClick={handleClickSendMessage}
-          disabled={readyState !== ReadyState.OPEN}
-        >
-          Send
-        </button>
+          <button
+            className="border-2 rounded-lg border-gray-700 w-60 ml-2 hover:border-green-700"
+            type="submit"
+            disabled={readyState !== ReadyState.OPEN}
+          >
+            Send
+          </button>
+        </form>
       </div>
       <br></br>
 
       {/* {lastMessage ? <span>Last message: {lastMessage.data}</span> : null} */}
       <div>
         <ul>
-          {messageHistory.slice().reverse().map((message, idx) => (
-            <div className={message.origin ? "" : "text-right"} key={idx}>
-              {message ? message.data : null}
-            </div>
-          ))}
+          {messageHistory
+            .slice()
+            .reverse()
+            .map((message, idx) => (
+              <div className={message.origin ? "" : "text-right"} key={idx}>
+                {message ? message.data : null}
+              </div>
+            ))}
         </ul>
       </div>
     </div>
