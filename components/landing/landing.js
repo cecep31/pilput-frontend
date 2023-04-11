941943547f3d0e1544594db5c9f9120c8d8a994c
cecep31 <cecepjanuardi31@gmail.com>
2023-04-11 13:38:07 +0700

    change link

--- components/landing/landing.js
+++ components/landing/landing.js
@@ -16,8 +16,8 @@ const Landing = () => {
               Programmer
             </h2>
             <div className="flex items-center justify-center mt-4">
-              <Link
-                href="/chat/global"
+              <Link target="_blank"
+                href="https://www.linkedin.com/in/cecep31/"
                 className="px-4 py-2 my-2 text-gray-800 uppercase bg-transparent border-2 border-gray-800 md:mt-16 dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 dark:text-white hover:bg-gray-800 hover:text-white text-md"
               >
                 CONNECT WITH ME
